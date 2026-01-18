2f976e6bde410b843b25156c36e1781931d83cce
aramay <kasper_341@hotmail.com>
2026-01-18 10:36:33 -0800

    setup DB connection with drizzle

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
+process.loadEnvFile()
+
+function envOrThrow(url: string) {
+    const value = process.env[url]
+    if (!value) {
+        throw new Error(`Environment variable ${value} is not set`)
+    }
+    return value
+}
+
 type APIConfig = {
     fileServerHits: number;
+    dbURL: string;
 };
 
 export const config: APIConfig = {
-    fileServerHits: 0
+    fileServerHits: 0,
+    dbURL: envOrThrow("DB_URL")
 }
